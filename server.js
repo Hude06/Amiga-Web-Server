82086615193af80903db7776c52d9db34e227c9e
judeallanhill <judeallanhill@icloud.com>
2024-04-13 10:33:41 -0700

    fix

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
+import bodyParser from 'body-parser';
 const app = express();
 const port = 3000;
 // Load existing data from JSON file, if any
