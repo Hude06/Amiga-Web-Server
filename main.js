0e94b9bcc1a9807c9f672a6c7177a497242ea66e
judeallanhill <judeallanhill@icloud.com>
2024-04-12 20:55:30 -0700

    Final Draft, Got drawing Points working

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
-
+fetch('./points.json')
+  .then(response => response.json())
+  .then(data => {
+    // Use the JSON data here
+    for (let i = 0; i < data.cords.length; i++) {
+        points.push(new Point(data.cords[i].latitude,data.cords[i].longitude))
+    }
+    for (let i = 0; i < points.length; i++) {
+        points[i].plot();
+     }
+  })
+  .catch(error => console.error('Error fetching JSON:', error));
 // Real-world latitude and longitude points
 class Point {
-    constructor(latitude,longitude,scalledX,scalledY) {
+    constructor(latitude,longitude) {
         this.latitude = latitude;
         this.longitude = longitude;
-
-        this.scalledX = scalledX
-        this.scalledY = scalledY
+        this.scalledX = 0;
+        this.scalledY = 0;
     }
     plot() {
         this.scalledX = (this.longitude + 180) * (canvas.width / 360);
@@ -17,21 +27,28 @@ class Point {
     }
 }
 
-let point1 = new Point(10.7128,-74.0060)
-let point2 = new Point(35.6895,139.6917)
-let point3 = new Point(36.57227534133874,-119.34141253053983)
-
-let points = [point1,point2,point3]
+let points = []
 function drawLine(point1,point2) {
     ctx.beginPath(); // Start a new path
     ctx.moveTo(point1.scalledX, point1.scalledY); // Move the pen to (30, 50)
     ctx.lineTo(point2.scalledX, point2.scalledY); // Draw a line to (150, 100)
     ctx.stroke();
 }
-for (let i = 0; i < points.length; i++) {
-   points[i].plot();
+function calcDist(point1,point2) {
+    let xDist = point1.scalledX - point2.scalledX;
+    let yDist = point1.scalledY - point2.scalledY;
+    return { x: Math.abs(xDist), y: Math.abs(yDist) };
 }
-drawLine(points[0],points[1])
-drawLine(points[0],points[2])
+setTimeout(() => {
+    for (let i = 0; i < points.length; i++) {
+        if (i-1 !== -1) {
+            console.log(calcDist(points[i-1],points[i]))
+            drawLine(points[i-1],points[i])
 
+        } else {
+            console.log("0")
+        }
+    }
+}, 200);
+  
 
