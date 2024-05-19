9db7b25fa68af04220b94359ab1cc782f3b906dc
judeallanhill <judeallanhill@icloud.com>
2024-05-18 19:51:22 -0700

    New code

--- mainReader.py
+++ mainReader.py
@@ -11,7 +11,7 @@ url_post = "https://judemakes.dev/posts"
 
 def adjust_gps(latitude, longitude, accuracy):
     # Convert accuracy from degrees to meters
-    if accuracy:
+    if accuracy != 0:
         meters_accuracy = accuracy * 111139
         adjusted_lat = latitude + (accuracy / 111139)  # Adjust latitude
         adjusted_lon = longitude - (accuracy / (111139 * math.cos(math.radians(latitude))))  # Adjust longitude
@@ -19,8 +19,7 @@ def adjust_gps(latitude, longitude, accuracy):
     else:
         return {'latitude': latitude, 'longitude': longitude}
 
-accuracy = 0.000267  # Horizontal accuracy in degrees
-
+accuracy = 0
 while True:
     received_data = ser.readline().decode('utf-8').strip()
     if received_data == "Waiting for fix...":
@@ -30,7 +29,7 @@ while True:
         if len(splitData) >= 2:  # Check if splitData contains at least two elements
             latitude = float(splitData[0])  # Latitude is at index 1
             longitude = float(splitData[1])  # Longitude is at index 2
-            adjusted_gps = adjust_gps(latitude, longitude, accuracy)
+            adjusted_gps = adjust_gps(latitude, longitude,accuracy)
             print("Adjusted Latitude:", adjusted_gps['latitude'], "Original Latitude: ", latitude)
             print("Adjusted Longitude:", adjusted_gps['longitude'], "Original Longitude: ", longitude)
             new_data = {
@@ -38,9 +37,9 @@ while True:
                 "latitude": adjusted_gps['latitude'],
                 "longitude": adjusted_gps['longitude']
             }      
-            # with open("points.json", "a") as file:
-            #     json.dump(new_data, file)
-            #     file.write("\n")  # Append a newline after each JSON object
+            with open("points.json", "a") as file:
+                json.dump(new_data, file)
+                file.write("\n")  # Append a newline after each JSON object
         else:
             print("Invalid data format:", received_data)
     time.sleep(1)  # Add a 1-second delay between iterations
--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.get('/DroneOnline', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4300;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
