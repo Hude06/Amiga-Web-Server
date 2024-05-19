cde88d186beab3b131c94741c90f5d7d4b19a522
judeallanhill <judeallanhill@icloud.com>
2024-05-18 20:19:10 -0700

    Basic Fixes

--- mainReader.py
+++ mainReader.py
@@ -3,11 +3,11 @@ import sys
 import time
 import math
 import json
-
+import requests
 port = "/dev/tty.usbmodem11401"
 baudrate = 19200
 ser = serial.Serial(port, baudrate, timeout=0.001)
-url_post = "https://judemakes.dev/posts"
+url = 'http://apps.hude.earth:4300/ImOnline'
 
 def adjust_gps(latitude, longitude, accuracy):
     # Convert accuracy from degrees to meters
@@ -30,6 +30,7 @@ while True:
             latitude = float(splitData[0])  # Latitude is at index 1
             longitude = float(splitData[1])  # Longitude is at index 2
             adjusted_gps = adjust_gps(latitude, longitude,accuracy)
+            response = requests.get(url)
             print("Adjusted Latitude:", adjusted_gps['latitude'], "Original Latitude: ", latitude)
             print("Adjusted Longitude:", adjusted_gps['longitude'], "Original Longitude: ", longitude)
             new_data = {
@@ -37,9 +38,9 @@ while True:
                 "latitude": adjusted_gps['latitude'],
                 "longitude": adjusted_gps['longitude']
             }      
-            with open("points.json", "a") as file:
-                json.dump(new_data, file)
-                file.write("\n")  # Append a newline after each JSON object
+            # with open("points.json", "a") as file:
+            #     json.dump(new_data, file)
+            #     file.write("\n")  # Append a newline after each JSON object
         else:
             print("Invalid data format:", received_data)
     time.sleep(1)  # Add a 1-second delay between iterations
